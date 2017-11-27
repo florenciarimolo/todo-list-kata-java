d4d5e88406ab85e1f08e4c7953bc58070f6e8890
Eduard Maura <eduard.maura.i@fib.upc.edu>
2017-11-27 23:24:24 +0100

    Add fake class to set src main structure

---
+++ src/main/java/edu/inlab/katas/DeleteMeAfterRun.java
@@ -0,0 +1,5 @@
+package edu.inlab.katas
+
+public class DeleteMeAfterRun {
+
+}
--- src/test/java/edu/inlab/katas/DeleteMeAfterRunTest.java
+++ src/test/java/edu/inlab/katas/DeleteMeAfterRunTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ConfigTest {
+public class DeleteMeAfterRunTest {
     @Test
     public void test() {
         assertTrue(true);
